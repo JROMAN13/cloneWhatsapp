7396a8c68a028212277022393265f077188eb437
Jerson Roman <jroman1394@gmail.com>
2024-03-01 00:04:20 -0500

    Configuración babel y webpack, estructura de carpetas y maquetación Login

---
+++ src/app/index.js
@@ -0,0 +1,54 @@
+import "./styles/style.scss";
+// import image from "./assets/images/logo.svg";
+import {
+  createTransaction,
+  deleteATransaction,
+  getTransactions,
+} from "./services/transactionServices";
+import printTransactions from "./modules/printTransactions";
+import { getDataForm } from "./modules/getDataForm";
+
+//Actualizar las imágenes en el atributo src de las etiquetas
+let transactions = [];
+const logoImage = document.getElementById("logo");
+const transactionsContainer = document.getElementById("transactions");
+const form = document.getElementById("form");
+
+const switchers = [...document.querySelectorAll('.switcher')]
+
+switchers.forEach(item => {
+	item.addEventListener('click', function() {
+		switchers.forEach(item => item.parentElement.classList.remove('is-active'))
+		this.parentElement.classList.add('is-active')
+	})
+})
+
+logoImage.setAttribute("src", image);
+
+//Queremos listar los movimientos o trasacciones
+document.addEventListener("DOMContentLoaded", async () => {
+  transactions = await getTransactions();
+  printTransactions(transactionsContainer, transactions);
+});
+
+form.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const newTransaction = getDataForm(form);
+  const responseTransaction = await createTransaction(newTransaction);
+  transactions.push(responseTransaction.data);
+  printTransactions(transactionsContainer, transactions);
+  form.reset();
+});
+
+document.addEventListener("click", async (event) => {
+  if (event.target.classList.contains("delete")) {
+    const id = event.target.getAttribute("name");
+    await deleteATransaction(id);
+    transactions = await getTransactions();
+    printTransactions(transactionsContainer, transactions);
+    }
+    
+    if (event.target.classList.contains("edit")) {
+        console.log("Quiero editar");
+    }
+});
