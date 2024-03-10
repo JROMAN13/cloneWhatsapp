6f4c6c912c2704ba641a6e788f0da625dc04d68a
estefania.martinez@est.iudigital.edu.co <estefania.martinez@est.iudigital.edu.co>
2024-03-10 12:27:33 -0500

    se modifica configuración de Babel, se crean los endpoints de mensajes y se crea la promesa

--- src/app/services/data.js
+++ src/app/services/data.js
@@ -3,5 +3,9 @@ const URL_BASE = "http://localhost:3000/";
 export const endpoints = {
   transactions: `${URL_BASE}usuarios`,
   transaction: (id) => `${URL_BASE}usuarios/${id}`,
-  getAnUser: (celular, contraseña) => `${URL_BASE}usuarios?celular=${celular}&contraseña=${contraseña}`
+  getAnUser: (celular, contraseña) =>
+    `${URL_BASE}usuarios?celular=${celular}&contraseña=${contraseña}`,
+  messages: `${URL_BASE}mensajes`,
+  messagesStarted: (senderUser) =>`${URL_BASE}mensajes?senderUser=${senderUser}`,
+  messageReceived: (recipientUser)=>`${URL_BASE}mensajes?recipientUser=${recipientUser}`,
 };
--- src/app/services/userServices.js
+++ src/app/services/userServices.js
@@ -1,10 +1,25 @@
 import axios from "axios";
+import { endpoints } from "./data";
 export const getUser = async (url) => {
     try {
       const { data } = await axios.get(url);
       return data[0];
     } catch (error) {
-      console.log(error);
+      console.error(error);
       return null;
     }
+
   };
+
+ export const getUserConversations= async(idUser)=> {
+  try {
+    const started = await axios.get(endpoints.messagesStarted(idUser));
+    const received = await axios.get(endpoints.messageReceived(idUser));
+    return [...started.data,...received.data];
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+ }
+
+
